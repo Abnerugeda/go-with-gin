55e5e5f0900715b3433fd1ae7f77d03ca8634853
Abner Ugeda <devabnerugeda@gmail.com>
2024-12-27 23:35:39 -0300

    test: criando test para a rota de search

--- tests/main_test.go
+++ tests/main_test.go
@@ -7,13 +7,29 @@ import (
 	"testing"
 
 	"github.com/abnerugeda/go-with-gin/controllers"
+	"github.com/abnerugeda/go-with-gin/database"
+	"github.com/abnerugeda/go-with-gin/models"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
+var ID int
+
 func SetupRotasTeste() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
 	rotas := gin.Default()
 	return rotas
+}
+
+func CriaAlunoMock() {
+	aluno := models.Aluno{Nome: "Nome aluno teste", CPF: "12312312312", RG: "123123123"}
+	database.DB.Create(&aluno)
+	ID = int(aluno.ID)
+}
+
+func DeletaAlunoMock() {
+	var aluno models.Aluno
+	database.DB.Delete(&aluno, ID)
 }
 
 func TestVerificaStatusCodeSaudacoes(t *testing.T) {
@@ -27,4 +43,29 @@ func TestVerificaStatusCodeSaudacoes(t *testing.T) {
 	mockResposta := `{"API DIZ: ":"E ai abner, tudo bem?"}`
 	respostaBody, _ := io.ReadAll(resposta.Body)
 	assert.Equal(t, mockResposta, string(respostaBody))
+}
+
+func TestListandoAlunosHandler(t *testing.T) {
+	database.ConnectDB()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupRotasTeste()
+	r.GET("/alunos", controllers.FindAlunos)
+	req, _ := http.NewRequest("GET", "/alunos", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+
+	assert.Equal(t, http.StatusOK, resposta.Code)
+}
+
+func TestBuscaAlunoCPF(t *testing.T) {
+	database.ConnectDB()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupRotasTeste()
+	r.GET("/alunos/cpf/:cpf", controllers.SearchAlunoByCPF)
+	req, _ := http.NewRequest("GET", "/alunos/cpf/12312312312", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
 }
