b76c599a6cce4f056da227168bb0e38ca10db3d8
Abner Ugeda <devabnerugeda@gmail.com>
2024-12-27 22:38:01 -0300

    feat: adicionando validator e validando alunos

--- controllers/alunos.controller.go
+++ controllers/alunos.controller.go
@@ -48,6 +48,13 @@ func CreateAluno(c *gin.Context) {
 		return
 	}
 
+	if err := models.ValidaDadosAluno(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
 	database.DB.Create(&aluno)
 	c.JSON(http.StatusOK, aluno)
 }
@@ -68,6 +75,13 @@ func EditAluno(c *gin.Context) {
 	database.DB.First(&aluno, id)
 
 	if err := c.ShouldBindJSON(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if err := models.ValidaDadosAluno(&aluno); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
 		})
